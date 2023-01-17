a990d34d069db74cf6ea646bdd69092481c86f96
golamrabbani93 <golamrabbani9373@gmail.com>
2023-01-17 11:34:54 +0600

    install express mongodb dot env jwt

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello Contact List');
+const express = require('express');
+const cors = require('cors');
+var jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+	res.send('Contact-List server is runing');
+});
+
+app.listen(port, () => {
+	console.log(`Contact-List server is running on ${port}`);
+});
